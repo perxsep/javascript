7f8c39420f5d7e432f201ea4bf677834d72419df
perxsep <piero.sepulveda.vallejos@gmail.com>
2022-04-07 11:29:37 -0400

    des

---
+++ PIERO_SEPULVEDA_JSPREDICCION..js
@@ -0,0 +1,19 @@
+function myBirthYearFunc(){
+        console.log("Nací en " + 2005);
+    }
+    /console log indica que naci en 2005
+
+    function myBirthYearFunc(2005){
+            console.log("Nací en " + 2005);
+        }
+        /console log indica el año en que naci aplicada la funcion
+
+        function add(10,20){
+                console.log("¡Sumando números!");
+                console.log("num1 is: " + 10);
+                console.log("num2 is: " + 20);
+                var sum = 10 + 20;
+                console.log(sum);
+            }
+            
+            /console log indica la suma de mis dos variables
