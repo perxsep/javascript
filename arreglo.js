02b88f1f86695da0c138404ae21a25eac3879589
perxsep <piero.sepulveda.vallejos@gmail.com>
2022-04-12 11:00:17 -0400

    diagram t

--- arreglo.js
+++ arreglo.js
@@ -1,15 +1,17 @@
 //fragmento Nº2:
 function muestraListaDeCompras() {
     var produce = ["manzanas", "naranjas"];
-    var frozen = ["brócoli", "helado"];
-    frozen.push("croqueta de papa");
-    console.log(frozen);
+    var frozen = ["brócoli", "helado"];
+    frozen.push("croqueta de papa");
+    console.log(frozen);
 }
 //variables:produce-dato:["manzanas", "naranjas"]
 //variables:frozen-dato:["brócoli", "helado"]
 //fragmento nº3:
-*/var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
 movieLibrary.push("Zoro");
 movieLibrary[1] = "Beetlejuice";
 console.log(movieLibrary);
-//variable:movielibrary-dato:(bambi,e.t,toy story)
+//variable:movielibrary-dato:["Bambi", "E.T.", "Toy Story"]
+
+ 
