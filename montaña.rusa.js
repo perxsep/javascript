0d9de73ce1af640446507bc60fbd618b60b9dc89
perxsep <piero.sepulveda.vallejos@gmail.com>
2022-04-21 09:03:27 -0400

    montaña.rusa

---
+++ montaña.rusa.js
@@ -0,0 +1,11 @@
+var alturaNiño = 45;
+function muestraSiElNiñoPuedeSubirALaMontañaRusa () {
+    if (alturaNiño >= 52) {
+        console.log ("¡Súbete, chico!")
+    }
+    else {
+        alturaNiño < 52; {
+            console.log ("Lo siento, chico. Tal vez el próximo año")
+        }
+    }
+}
