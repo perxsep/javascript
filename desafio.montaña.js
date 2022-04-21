fd8b42afc1c12dc6729ab344511a0bd3436c86c3
perxsep <piero.sepulveda.vallejos@gmail.com>
2022-04-21 09:02:03 -0400

    desafio.montaña.js

---
+++ desafio.montaña.js
@@ -0,0 +1,11 @@
+var alturaNiño = 44;    
+}
+if (x > 52) {    
+    console.log("!subete chico!");
+}
+else if (x < 52) {    
+    console.log("Lo siento, chico. Tal vez el próximo año ");
+}
+else if(x == 52) {
+    console.log("!subete chico!");
+}
